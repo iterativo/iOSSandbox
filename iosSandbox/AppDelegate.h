d901bc5823b0a74992c82280f7a2e0af7162d854
Rafael <torresrafael@gmail.com>
2015-04-17 21:37:04 -0500

    Initial Commit

---
+++ iosSandbox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iosSandbox
+//
+//  Created by Rafael on 4/17/15.
+//  Copyright (c) 2015 slikcode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iosSandbox/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iosSandbox
+//
+//  Created by Rafael on 4/17/15.
+//  Copyright (c) 2015 slikcode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
