38e752748be56d537ed6702b1cafd0edf4190617
Rafael <torresrafael@gmail.com>
2015-04-18 09:28:36 -0500

    Initial commit

---
+++ iosSandbox/RestService.h
@@ -0,0 +1,13 @@
+//
+//  HttpService.h
+//  iosSandbox
+//
+//  Created by Rafael on 4/17/15.
+//  Copyright (c) 2015 slikcode. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RestService : NSObject
+
+@end
---
+++ iosSandbox/RestService.m
@@ -0,0 +1,13 @@
+//
+//  HttpService.m
+//  iosSandbox
+//
+//  Created by Rafael on 4/17/15.
+//  Copyright (c) 2015 slikcode. All rights reserved.
+//
+
+#import "RestService.h"
+
+@implementation RestService
+
+@end
